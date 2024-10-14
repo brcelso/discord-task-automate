a18127907628c81ff80b2569377f02c33be606f5
celso <celso.sjunior@outlook.com>
2024-10-14 15:40:50 -0300

    Update send_discord_message.py

--- send_discord_message.py
+++ send_discord_message.py
@@ -8,11 +8,12 @@ load_dotenv()
 
 # Pegando os valores das variáveis do ambiente (GitHub Secrets)
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))
+CHANNEL_ID_1 = int(os.getenv('DISCORD_CHANNEL_ID_1'))
+CHANNEL_ID_2 = int(os.getenv('DISCORD_CHANNEL_ID_2'))
 
 # Verificar se as variáveis foram carregadas corretamente
-if not TOKEN or not CHANNEL_ID:
-    raise ValueError("As variáveis de ambiente DISCORD_BOT_TOKEN ou DISCORD_CHANNEL_ID não foram encontradas.")
+if not TOKEN or not CHANNEL_ID_1 or not CHANNEL_ID_2:
+    raise ValueError("As variáveis de ambiente DISCORD_BOT_TOKEN, DISCORD_CHANNEL_ID_1 ou DISCORD_CHANNEL_ID_2 não foram encontradas.")
 
 # Configurar o bot
 intents = discord.Intents.default()
@@ -21,17 +22,30 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f'Bot {bot.user} está online!')
-    channel = bot.get_channel(CHANNEL_ID)
-    if channel:
-        try:
-            await channel.send('Olá! Lembre-se de dar comida para os Dogs!')
-            print(f'Mensagem enviada para o canal {channel.name}.')
-        except Exception as e:
-            print(f'Erro ao enviar mensagem: {e}')
-    else:
-        print('Canal não encontrado.')
 
-    # Encerrar o bot após enviar a mensagem
+    # Enviar mensagem para o primeiro canal
+    channel_1 = bot.get_channel(CHANNEL_ID_1)
+    if channel_1:
+        try:
+            await channel_1.send('Olá! Lembre-se de dar comida para os Dogs!')
+            print(f'Mensagem enviada para o canal {channel_1.name}.')
+        except Exception as e:
+            print(f'Erro ao enviar mensagem para o canal {channel_1.name}: {e}')
+    else:
+        print('Canal 1 não encontrado.')
+
+    # Enviar mensagem para o segundo canal
+    channel_2 = bot.get_channel(CHANNEL_ID_2)
+    if channel_2:
+        try:
+            await channel_2.send('Olá! Lembre-se de dar comida para os Dogs!')
+            print(f'Mensagem enviada para o canal {channel_2.name}.')
+        except Exception as e:
+            print(f'Erro ao enviar mensagem para o canal {channel_2.name}: {e}')
+    else:
+        print('Canal 2 não encontrado.')
+
+    # Encerrar o bot após enviar as mensagens
     await bot.close()
 
 # Iniciar o bot
